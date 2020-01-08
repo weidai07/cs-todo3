942a22e3215904e260a3a2ae043f3ce865a1649b
Anthony Golovin and Wei Dai <wei@>
2020-01-08 10:08:04 -0800

    updated todo list

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -27,5 +27,31 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }    
+
   }
 }
